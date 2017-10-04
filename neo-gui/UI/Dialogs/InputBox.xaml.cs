541af254d388fcf10e88853eea80ba06a07d8a0c
Justin Gaffney <justingaffney@hotmail.com>
2017-10-04 22:46:06 +1000

    Ported InputBox and InformationBox to WPF

---
+++ neo-gui/UI/Dialogs/InputBox.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace Neo.UI.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for InputBox.xaml
+    /// </summary>
+    public partial class InputBox
+    {
+        private string inputResult;
+
+        private InputBox(string text, string caption, string content)
+        {
+            InitializeComponent();
+
+            this.Title = caption;
+            this.GroupBox.Header = text;
+            this.InputTextBox.Text = content;
+        }
+
+        public static string Show(string text, string caption, string content = "")
+        {
+            var dialog = new InputBox(text, caption, content);
+
+            dialog.ShowDialog();
+
+            return dialog.inputResult;
+        }
+
+        private void OkClicked(object sender, RoutedEventArgs e)
+        {
+            this.inputResult = this.InputTextBox.Text;
+
+            this.Close();
+        }
+
+        private void CancelClicked(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
