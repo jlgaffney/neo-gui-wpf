82898f62fde4304e8510ae67dc8866d7dbdd25de
Justin Gaffney <justingaffney@hotmail.com>
2017-10-29 13:46:22 +1000

    Localised AccountType enum when display on the UI

--- neo-gui/UI/AccountType.cs
+++ neo-gui/UI/AccountType.cs
@@ -1,12 +1,20 @@
-﻿namespace Neo.UI
+﻿using System.ComponentModel;
+using Neo.UI.Base.Converters;
+using Neo.UI.Base.Localization;
+using Neo.UI.Base.Resources;
+
+namespace Neo.UI
 {
-    // TODO Localise enum
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum AccountType
     {
+        [LocalizedDescription(nameof(EnumStrings.Standard), typeof(EnumStrings))]
         Standard,
 
+        [LocalizedDescription(nameof(EnumStrings.NonStandard), typeof(EnumStrings))]
         NonStandard,
 
+        [LocalizedDescription(nameof(EnumStrings.WatchOnly), typeof(EnumStrings))]
         WatchOnly
     }
 }
