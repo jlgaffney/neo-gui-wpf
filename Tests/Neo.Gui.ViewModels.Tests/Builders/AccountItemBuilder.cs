7c6fff7f3de25fc2b78d46416aed1ecff154c1bd
Justin Gaffney <justingaffney@hotmail.com>
2018-01-06 19:00:24 +1000

    Merge changes from dev branch into master branch (#123)
    * AccountViewModel clean up according with the Regions structure
    
    * Initial tests on AccountViewModels
    
    #81, #46
    
    * Add several tests to AccountViewModelTests
    
    * Separated interfaces and implementation classes to separate namespaces, general code clean up (#117)
    
    * AccountsViewModels coverd by UnitTests
    
    #46
    
    * Organize the Usings in WalletController
    
    * Adjust to new Interfaces
    
    * Added Travis CI support to automatically run unit test suite on commits (#122)
    
    * Added Travis CI configuration YAML file
    
    * Added Travis CI build status badge .svg image to README

---
+++ Tests/Neo.Gui.ViewModels.Tests/Builders/AccountItemBuilder.cs
@@ -0,0 +1,60 @@
+﻿using Neo.Gui.Base.Data;
+
+namespace Neo.Gui.ViewModels.Tests.Builders
+{
+    public class AccountItemBuilder
+    {
+        private AccountType accountType = AccountType.Standard;
+        private string labelInternal = "accountLabel";
+        private UInt160 hashInternal;
+        private Fixed8 neoBalance = Fixed8.Zero;
+
+        public AccountItemBuilder WithLabel(string label)
+        {
+            this.labelInternal = label;
+            return this;
+        }
+
+        public AccountItemBuilder WithHash(string hash)
+        {
+            this.hashInternal = UInt160.Parse(hash);
+            return this;
+        }
+
+        public AccountItemBuilder StandardAccount()
+        {
+            this.accountType = AccountType.Standard;
+            return this;
+        }
+
+        public AccountItemBuilder NonStandardAccount()
+        {
+            this.accountType = AccountType.NonStandard;
+            return this;
+        }
+
+        public AccountItemBuilder WatchOnlyAccount()
+        {
+            this.accountType = AccountType.WatchOnly;
+            return this;
+        }
+
+        public AccountItemBuilder AccountWithNeoBalance()
+        {
+            this.neoBalance = new Fixed8(1);
+            return this;
+        }
+
+        public AccountItem Build()
+        {
+            var account = new AccountItem(this.labelInternal, this.hashInternal, this.accountType);
+
+            if (this.neoBalance != Fixed8.Zero)
+            {
+                account.Neo = this.neoBalance;
+            }
+
+            return account;
+        }
+    }
+}
