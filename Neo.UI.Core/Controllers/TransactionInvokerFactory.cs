6f212189e5247c1ae84a55c5645eed7041a774e9
Paulo Aboim Pinto <aboimpinto@gmail.com>
2018-01-24 16:34:45 +0100

    add Invokers configurations into the TransactionInvokerFactory

--- Neo.UI.Core/Controllers/TransactionInvokerFactory.cs
+++ Neo.UI.Core/Controllers/TransactionInvokerFactory.cs
@@ -21,7 +21,11 @@ namespace Neo.UI.Core.Controllers
         {
             var configDictionary = new Dictionary<InvocationTransactionType, ITransactionConfiguration>
             {
-                { invocationTransactionType, new AssetRegistrationTransactionConfiguration { WalletController = walletController, InvocationTransactionType = invocationTransactionType, AssetRegistrationTransactionParameters = assetRegistrationParameters } }
+                { invocationTransactionType, new AssetRegistrationTransactionConfiguration { WalletController = walletController, InvocationTransactionType = invocationTransactionType, AssetRegistrationTransactionParameters = assetRegistrationParameters }},
+                { invocationTransactionType, new AssetTransferTransactionConfiguration { WalletController = walletController, InvocationTransactionType = invocationTransactionType, AssetTransferTransactionParameters = assetTransferTransactionParameters }},
+                { invocationTransactionType, new DeployContractTransactionConfiguration { WalletController = walletController, InvocationTransactionType = invocationTransactionType, DeployContractTransactionParameters = deployContractTransactionParameters }},
+                { invocationTransactionType, new ElectionTransactionConfiguration { WalletController = walletController, InvocationTransactionType = invocationTransactionType, ElectionTransactionParameters = electionTransactionParameters}},
+                { invocationTransactionType, new VotingTransactionConfiguration { WalletController = walletController, InvocationTransactionType = invocationTransactionType, VotingTransactionParameters = votingTransactionParameters}}
             };
 
             foreach(var invoker in transactionInvokers)
