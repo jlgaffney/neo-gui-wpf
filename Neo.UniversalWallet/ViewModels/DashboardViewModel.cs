1a99c17eeb03bfd9f219b96613475204dfdf1e62
Paulo Aboim Pinto <aboimpinto@live.com>
2018-02-27 19:06:39 +0100

    Including the Receiving QRCode and in the Dashboard Screen and redo the top part of the screen

--- Neo.UniversalWallet/ViewModels/DashboardViewModel.cs
+++ Neo.UniversalWallet/ViewModels/DashboardViewModel.cs
@@ -1,4 +1,6 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using Neo.UniversalWallet.Messages;
 
@@ -6,13 +8,33 @@ namespace Neo.UniversalWallet.ViewModels
 {
     public class DashboardViewModel : ViewModelBase
     {
+        private string selectedAsset;
+
         #region Public Properties 
+        public ObservableCollection<string> Assets { get; private set; }
+
+        public string SelectedAsset
+        {
+            get
+            {
+                return this.selectedAsset;
+            }
+            set
+            {
+                this.selectedAsset = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
         public RelayCommand<string> AssetSelectionCommand { get; private set; }
         #endregion
 
         #region Constructor 
         public DashboardViewModel()
         {
+            this.Assets = new ObservableCollection<string> { "NEO", "GAS", "RPX" };
+            this.SelectedAsset = this.Assets.First();
+
             this.AssetSelectionCommand = new RelayCommand<string>(this.HandleAssetSelection);
         }
         #endregion
