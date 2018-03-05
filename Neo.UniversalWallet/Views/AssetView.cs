4ca2fdc54a856ab1076dff21aeca7e65bbde8f55
Paulo Aboim Pinto <aboimpinto@live.com>
2018-03-05 21:19:47 +0100

    Adding some missing items that were lost during the merge

---
+++ Neo.UniversalWallet/Views/AssetView.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Neo.UniversalWallet.Views
+{
+    public partial class AssetView : Page
+    {
+        public AssetView()
+        {
+            InitializeComponent();
+        }
+    }
+}
